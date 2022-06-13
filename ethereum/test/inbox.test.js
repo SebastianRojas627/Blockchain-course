07f374c737cd127c830cfe3260b9f82e0ea27209
Sebastian Rojas <sebosinaga@gmail.com>
2022-06-12 20:34:44 -0400

    [10-06-22]Task1 - Environment Configuration

---
+++ ethereum/test/inbox.test.js
@@ -0,0 +1,33 @@
+const Inbox = artifacts.require('Inbox')
+
+contract('Inbox', accounts => {
+
+    it('getMessage', async() => {
+
+        const instance = await Inbox.deployed();
+        const message = await instance.getMessage.call();
+        assert.equal(message, 'Hi');
+
+    });
+
+    it('setMessage', async() => {
+
+        const instance = await Inbox.deployed();
+        await instance.setMessage('Hi Sebas', {from: accounts[0]});
+        const message = await instance.getMessage.call();
+        assert.equal(message, 'Hi Sebas')
+
+    });
+
+    it('setMessage should not change var message', async () => {
+
+        try {
+        const instance = await Inbox.deployed();
+        await instance.setMessage('just Owner', {from: accounts[1]});
+        } catch (e) {
+            assert.equal(message, 'Only owner can access the message');
+        }
+
+    })
+
+})
