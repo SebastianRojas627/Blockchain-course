52c1c1f17695971a89c07aacce1c49944950ce78
Sebastian Rojas <sebosinaga@gmail.com>
2022-06-17 10:20:37 -0400

    [13-06-22] Task1 Extract Network Data

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,38 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { connectWallet, initialize } from "./ethereum/web3";
+import contractLottery from ".ethereum/abis/Lottery.json";
 
 function App() {
 
-  useEffect( () => {
+  useEffect( () => { //cuando el componente nace
     //@ts-ignore
     if(window.web3) {
       initialize()
     }
   }, [])
+
+  const loadBlockchainData = async () => {
+
+    //@ts-ignore
+    const Web3 = window.web3;
+
+    // Netowork id: Rinkeby:4, Ganache:5777, BSC:97
+    const networkData = contractLottery.networks['5777'];
+    console.log('networkData: ', networkData);
+
+    if(networkData) {
+
+      const abi = contractLottery.abi;
+      const address = networkData.address;
+      console.log('address: ', address)
+
+      const contractDeployed = new Web3.eth.Contract(abi, address);
+
+      const players = await contractDeployed.methods.getPlayers().call();
+      console.log('players: ', players, players.length)
+    }
+  }
 
   return (
     <div className="App">
@@ -21,6 +44,7 @@ function App() {
         </p>
         <p>Hi React, Truffle, Firebase</p>
         <button onClick={() => connectWallet()}>Connect</button>
+        <button onClick={() => loadBlockchainData()}>Load</button>
       </header>
     </div>
   );
