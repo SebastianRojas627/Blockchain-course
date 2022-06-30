d160adce624cf61d85554fda7c76b8a90aa58165
Sebastian Rojas <sebosinaga@gmail.com>
2022-06-30 00:17:43 -0400

    [29-06-22] Task 1 Internal functions in ERC20

--- src/ethereum/contracts/ERC20.sol
+++ src/ethereum/contracts/ERC20.sol
@@ -40,7 +40,7 @@ contract ERC20 is IERC20, IERC20Metadata{
 
     function transfer(address to, uint256 amount) public override returns(bool) {
         address owner = msg.sender;
-        _tranfer(owner, to, amount);
+        _transfer(owner, to, amount);
         return true;
     }
 
@@ -57,8 +57,46 @@ contract ERC20 is IERC20, IERC20Metadata{
     function transferFrom(address from, address to, uint256 amount) external override returns(bool) {
         address spender = msg.sender;
         _spendAllowance(from, spender, amount);
-        _trasnfer(from, to, amount);
+        _transfer(from, to, amount);
         return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0), "ERC20: transfer from the zero address.");
+        require(to != address(0), "ERC20: transfer to the zero address.");
+
+        uint256 fromBalance = _balance[from];
+        require(fromBalance >= amount, "ERC: transfer amount exceeds balance");
+
+        unchecked {
+            _balance[from] = fromBalance - amount;
+        }
+
+        _balance[to] += amount;
+
+        emit Transfer(from, to, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from the zero address.");
+        require(spender != address(0), "ERC20: approve to the zero address.");
+
+        _allowance[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+
+    }
+
+    function _spendAllowance(address owner, address spender, uint256 amount) internal {
+
+        uint256 currentAllowance = allowance(owner, spender);
+
+        if(currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "ERC20: insufficient allowance");
+
+            unchecked {
+            _approve(owner, spender, currentAllowance - amount);
+            }
+        }
     }
 
 }
